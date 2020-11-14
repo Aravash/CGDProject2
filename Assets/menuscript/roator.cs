edab696a7f347a6475c42757e0e61bc5ec88b3ec
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-11-14 17:55:47 +0000

    basic menu

---
+++ Assets/menuscript/roator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roator : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0f, speed* Time.deltaTime, 0f);
+    }
+}
