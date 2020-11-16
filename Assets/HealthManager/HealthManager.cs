acae054e11610092afb3b2c24576b0f712701ed0
Cameron <cameron2.bell@live.uwe.ac.uk>
2020-11-16 20:04:57 +0000

    Added Gradient to health bars and changed the look a little
    Currently the code needs cleaning up but is fine for demo

--- Assets/HealthManager/HealthManager.cs
+++ Assets/HealthManager/HealthManager.cs
@@ -7,9 +7,17 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
 
+
+
     Slider P1HPBar;
     Slider P2HPBar;
     [SerializeField] Gradient gradient;
+
+    Image imageP1;
+    Image imageP2;
+
+    Image P1Medic;
+    Image P2Medic;
 
     float MaxHP = 100;
     float currentP1HP;
@@ -23,41 +31,56 @@ public class HealthManager : MonoBehaviour
 
         P1HPBar = GameObject.FindGameObjectWithTag("Player1_HPBar").GetComponent<Slider>();
         P2HPBar = GameObject.FindGameObjectWithTag("Player2_HPBar").GetComponent<Slider>();
+
+        //Need to clean this up
+        imageP1 = GameObject.Find("FillP1").GetComponent<Image>();
+        imageP2 = GameObject.Find("FillP2").GetComponent<Image>();
+        P1Medic = GameObject.Find("P1").GetComponent<Image>();
+        P2Medic = GameObject.Find("P2").GetComponent<Image>();
         SetHPBars();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void SetHPBars()
     {
         P1HPBar.maxValue = MaxHP;
         P1HPBar.value = currentP1HP;
-
         P2HPBar.maxValue = MaxHP;
         P2HPBar.value = currentP2HP;
+
+        imageP1.color = gradient.Evaluate(1f);
+        P1Medic.color = imageP1.color;
+        imageP2.color = gradient.Evaluate(1f);
+        P2Medic.color = imageP2.color;
+
     }
 
     public void ChangeHP(float damage, int player_id)
     {
-        if(player_id == 0)
+        if (player_id == 0)
         {
             currentP2HP -= damage;
             P2HPBar.value = currentP2HP;
+            imageP2.color = gradient.Evaluate(P2HPBar.normalizedValue);
+            P2Medic.color = imageP2.color;
         }
-        else if (player_id == 1)
+        if (player_id == 1)
         {
             currentP1HP -= damage;
             P1HPBar.value = currentP1HP;
+            imageP1.color = gradient.Evaluate(P1HPBar.normalizedValue);
+            P1Medic.color = imageP1.color;
         }
     }
 
     public bool checkEndGame()
     {
-        if(currentP1HP <= 0 || currentP2HP <= 0)
+        if (currentP1HP <= 0 || currentP2HP <= 0)
         {
             return true;
         }
